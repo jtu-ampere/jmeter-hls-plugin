2958fcd3b1ec03e7dd48f8feae92a36811b15e13
jtu-ampere <113143099+jtu-ampere@users.noreply.github.com>
2023-03-10 11:13:47 -0800

    Update vod-run.sh

--- vod-run.sh
+++ vod-run.sh
@@ -3,12 +3,16 @@
 # Run JMeter-HLS-Plug Container image with options
 
 NAME="jmeter-hls-plugin"
-JMETER_VERSION=${JMETER_VERSION:-"latest"}
+JMETER_VERSION=${JMETER_VERSION:-"5.4.3"}
+#JMETER_VERSION=${JMETER_VERSION:-"latest"}
 IMAGE="mrdojojo/jmeter-hls-plugin:${JMETER_VERSION}"
+WORKPATH="/home/altrauser/vod-benchmark"
 
 # Finally run
-echo ${JMETER_VERSION}
-echo ${PWD}
-echo ${IMAGE}
+echo " JMeter Version: ${JMETER_VERSION} "
+echo "PWD:  ${PWD}"
+echo " JMeter Image:   ${IMAGE}"
+echo "Parameters for JMeter:   ${@}"
+
 $podman run --rm --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
-sudo podman run --rm --cpuset-cpus 1-30 --pids-limit=-1 --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
+sudo podman run --rm --cpuset-cpus 1-30 --pids-limit=-1 --name ${NAME} -i -v ${PWD}:${WORKPATH}:Z  -w ${WORKPATH}  ${IMAGE} $@
